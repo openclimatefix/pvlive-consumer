94814a0095ac2ef5bbfe88042388408c054d92d0
peterdudfield <peter.dudfield@hotmail.com>
2022-03-22 22:09:07 +0000

    first commit, get in-day working

---
+++ gspconsumer/app.py
@@ -0,0 +1,204 @@
+""" Application for getting live GSP data
+
+1. Load GSP ids from database
+2. For each site, find the most recent data in a database
+3. Pull data from https://www.solar.sheffield.ac.uk/pvlive/, if more data is available.
+4. Save data to database - extra: check no duplicate data is added to the database
+"""
+
+import logging
+import os
+from datetime import datetime, timezone, timedelta
+from typing import List, Optional, Tuple
+
+import click
+from nowcasting_datamodel.connection import DatabaseConnection
+from nowcasting_datamodel.models.base import Base_Forecast
+from nowcasting_datamodel.models.gsp import LocationSQL, GSPYield, GSPYieldSQL
+from pvlive_api import PVLive
+
+from sqlalchemy.orm import Session
+
+import gspconsumer
+from gspconsumer.gsps import filter_gsps_which_have_new_data, get_gsps
+
+logging.basicConfig(
+    level=getattr(logging, os.getenv("LOGLEVEL", "DEBUG")),
+    format="[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option(
+    "--db-url",
+    default=None,
+    envvar="DB_URL",
+    help="The Database URL where forecasts will be saved",
+    type=click.STRING,
+)
+@click.option(
+    "--regime",
+    default="in-day",
+    envvar="REGIME",
+    help="regime of which to pull, either 'in-day' or 'day-after'",
+    type=click.STRING,
+)
+@click.option(
+    "--n_gsps",
+    default=10,
+    envvar="N_GSPS",
+    help="Number of gsps data to pull",
+    type=click.STRING,
+)
+def app(db_url: str, regime: str = "in-day", n_gsps: int = 339):
+    """
+    Run GSP consumer app, this collect GSP live data and save it to a database.
+
+    :param db_url: the Database url to save the PV system data
+    :param regime: if its "in-day" or "day-after"
+    :return:
+    """
+
+    logger.info(f"Running GSP Consumer app ({gspconsumer.__version__})")
+
+    n_gsps = int(n_gsps)
+
+    connection = DatabaseConnection(url=db_url, base=Base_Forecast, echo=True)
+    # connection.create_all()
+    with connection.get_session() as session:
+        # 1. Read list of GSP systems (from local file)
+        # and get their refresh times (refresh times can also be stored locally)
+        logger.debug("Read list of GSP from database")
+        gsps = get_gsps(session=session, n_gsps=n_gsps,regime=regime)
+        assert len(gsps) == n_gsps
+
+        # 2. Find most recent entered data (for each GSP) in OCF database,
+        # and filter depending on refresh rate
+        logger.debug(
+            "Find most recent entered data (for each GSP) in OCF database,"
+            "and filter GSP depending on refresh rate"
+        )
+        gsps = filter_gsps_which_have_new_data(gsps=gsps)
+        assert len(gsps) == n_gsps
+
+        # 3. Pull data
+        pull_data_and_save(gsps=gsps, session=session, regime=regime)
+
+
+def pull_data_and_save(
+    gsps: List[LocationSQL],
+    session: Session,
+    datetime_utc: Optional[None] = None,
+    regime: str = "in-day"
+):
+    """
+    Pull the gsp yield data and save to database
+
+    :param gsps: list of gsps to save
+    :param session: database sessions
+    :param provider: provider name
+    :param datetime_utc: datetime now, this is optional
+    """
+
+    pvlive = PVLive()
+
+    if datetime_utc is None:
+        datetime_utc = datetime.utcnow().replace(tzinfo=timezone.utc)  # add timezone
+
+    if regime == 'in-day':
+        start = datetime_utc - timedelta(hours=1)
+        end = datetime_utc + timedelta(minutes=30)
+    else:
+        start = datetime_utc.replace(hour=0,minute=0,second=0, microsecond=0) - timedelta(hours=24)
+        end = datetime_utc.replace(hour=0,minute=0,second=0, microsecond=0)
+
+    logger.info(f"Pulling data for {len(gsps)} GSP for {datetime_utc}")
+
+    all_gsps_yields_sql = []
+    for gsp in gsps:
+
+        gsp_yield_df = pvlive.between(
+            start=start,
+            end=end,
+            entity_type="gsp",
+            entity_id=120,
+            dataframe=True
+        )
+
+        logger.debug(
+            f"Processing {gsp.gsp_id} GSP, "
+            f"out of {len(gsps)}"
+        )
+
+        logger.debug(
+            f"Got {len(gsp_yield_df)} gsp yield for "
+            f"gsp {gsp.gsp_id} before filtering"
+        )
+
+        if len(gsp_yield_df) == 0:
+            logger.warning(f"Did not find any data for {gsp.gsp_id} for {start} to {end}")
+        else:
+
+            # filter by last
+            if gsp.last_gsp_yield is not None:
+                last_gsp_datetime = gsp.last_gsp_yield.datetime_utc.replace(
+                    tzinfo=timezone.utc
+                )
+                gsp_yield_df = gsp_yield_df[gsp_yield_df["datetime_gmt"] > last_gsp_datetime]
+
+                if len(gsp_yield_df) == 0:
+                    logger.debug(
+                        f"No new data available after {last_gsp_datetime}. "
+                        f"Last data point was {last_gsp_datetime}"
+                    )
+            else:
+                logger.debug(
+                    f"This is the first lot gsp yield data for "
+                    f"GSP {(gsp.gsp_id)}"
+                )
+
+            # need columns datetime_utc, solar_generation_kw
+            gsp_yield_df['solar_generation_kw'] = 1000 * gsp_yield_df['generation_mw']
+            gsp_yield_df['datetime_utc'] = gsp_yield_df['datetime_gmt']
+            gsp_yield_df = gsp_yield_df[["solar_generation_kw", "datetime_utc"]]
+            gsp_yield_df['regime'] = regime
+
+            # change to list of pydantic objects
+            gsp_yields = [GSPYield(**row) for row in gsp_yield_df.to_dict(orient="records")]
+
+            # change to sqlalamcy objects and add gsp systems
+            gsp_yields_sql = [gsp_yield.to_orm() for gsp_yield in gsp_yields]
+            for gsp_yield_sql in gsp_yields_sql:
+                gsp_yield_sql.location = gsp
+
+            logger.debug(
+                f"Found {len(gsp_yields_sql)} gsp yield for GSPs {gsp.gsp_id}"
+            )
+
+            all_gsps_yields_sql = all_gsps_yields_sql + gsp_yields_sql
+
+            if len(all_gsps_yields_sql) > 100:
+                # 4. Save to database - perhaps check no duplicate data. (for each GSP)
+                save_to_database(session=session, gsp_yields=all_gsps_yields_sql)
+                all_gsps_yields_sql = []
+
+    # 4. Save to database - perhaps check no duplicate data. (for each GSP)
+    save_to_database(session=session, gsp_yields=all_gsps_yields_sql)
+
+
+def save_to_database(session: Session, gsp_yields: List[GSPYieldSQL]):
+    """
+    Save GSP yield data to database
+
+    :param session: database session
+    :param gsp_yields: list of gsp data
+    """
+    logger.debug(f"Will be adding {len(gsp_yields)} gsp yield object to database")
+
+    session.add_all(gsp_yields)
+    session.commit()
+
+
+if __name__ == "__main__":
+    app()
---
+++ gspconsumer/gsps.py
@@ -0,0 +1,130 @@
+""" GSP functions """
+import logging
+from datetime import datetime, timedelta
+from typing import List, Optional
+
+from nowcasting_datamodel.models.gsp import Location, LocationSQL
+from nowcasting_datamodel.read.read import get_all_locations, get_location
+from nowcasting_datamodel.read.read_gsp import get_latest_gsp_yield
+from sqlalchemy.orm import Session
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_gsps(
+    session: Session, n_gsps: int = 339, regime: str = "in-day"
+) -> List[LocationSQL]:
+    """
+    Get PV systems
+
+    1. Load from database
+    2. load from local
+    3. add any pv systems not in database, by query pvoutput.org
+
+    :param session: database sessions
+    :param n_gsps: number of gsps, 0 is national then 1 to 338 is the gsps
+    :param regime: if its "in-day" or "day-after"
+    :return: list of gsps sqlalchemy objects
+    """
+    # load all pv systems in database
+
+    locations_sql_db: List[LocationSQL] = get_all_locations(session=session, gsp_ids=list(range(0,n_gsps)))
+    # locations_db = [Location.from_orm(pv_system) for pv_system in locations_sql_db]
+
+    logger.debug(f'Found {len(locations_sql_db)} locations in the database, should be {n_gsps}')
+
+    # get missing gsps
+    if len(locations_sql_db) != n_gsps:
+        gsp_ids_in_db = [location.gsp_id for location in locations_sql_db]
+        missing_gsp_ids = [gsp_id for gsp_id in range(0,n_gsps) if gsp_id not in gsp_ids_in_db]
+
+        logger.debug(f'There were {len(missing_gsp_ids)} missing gsp in the database')
+
+        new_locations = []
+        for gsp_id in missing_gsp_ids:
+            location = get_location(session=session,gsp_id=gsp_id)
+            new_locations.append(location)
+
+        all_locations = new_locations + locations_sql_db
+    else:
+        all_locations = locations_sql_db
+
+    assert len(all_locations) == n_gsps
+
+    all_locations = get_latest_gsp_yield(
+        session=session, append_to_gsps=True, gsps=all_locations, regime=regime
+    )
+
+    assert len(all_locations) == n_gsps, len(all_locations)
+
+    logger.debug(all_locations)
+
+    return all_locations
+
+
+def filter_gsps_which_have_new_data(
+    gsps: List[LocationSQL], datetime_utc: Optional[datetime] = None
+):
+    """
+    Filter gsps which have new data available
+
+    This is done by looking at the datestamp of last data pulled,
+    add then by looking at the pv system refresh time, we can determine if new data is available
+
+    sudo code:
+        if last_datestamp + refresh_interval > datetime_now
+            keep = True
+
+    Args:
+        gsps: list of gsps
+        datetime_utc: the datetime now
+
+    Returns: list of pv systems that have new data.
+
+    """
+
+    logger.info(
+        f"Looking at which GSP might have new data. "
+        f"Number of GSPs are {len(gsps)}"
+    )
+
+    if datetime_utc is None:
+        datetime_utc = datetime.utcnow()  # add timezone
+
+    logger.debug(gsps)
+
+    keep_gsps = []
+    for i, gsp in enumerate(gsps):
+
+        logger.debug(f"Looking at {i}th GSP, out of {len(gsps)} gsps")
+
+        last_yield = gsp.last_gsp_yield
+
+        if last_yield is None:
+            # there is no pv yield data for this pv system, so lets keep it
+            logger.debug(
+                f"There is no gsp yield data for GSP {gsp.gsp_id}, "
+                f"so will be getting data "
+            )
+            keep_gsps.append(gsp)
+        else:
+            next_datetime_data_available = (
+                timedelta(minutes=30) + last_yield.datetime_utc
+            )
+            if next_datetime_data_available < datetime_utc:
+                logger.debug(
+                    f"For gsp {gsp.gsp_id} as "
+                    f"last yield datetime is {last_yield.datetime_utc},"
+                    f"refresh interval is 30 minutes, "
+                    f"so will be getting data"
+                )
+                keep_gsps.append(gsp)
+            else:
+                logger.debug(
+                    f"Not keeping gsp {gsp.pv_system_id} as "
+                    f"last yield datetime is {last_yield.datetime_utc},"
+                    f"refresh interval is 30 minutes"
+                )
+
+    return keep_gsps
