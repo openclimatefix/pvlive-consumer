86014d42b8d6f22ecd0dd2ae0f35cdc162381214
Rachel Tipton <RACHELMTIPTON@GMAIL.COM>
2022-09-05 14:40:39 +0200

    fixing linting error

--- gsp_name_update/merge_labels.py
+++ gsp_name_update/merge_labels.py
@@ -14,7 +14,7 @@ print(old_df.head())
 
 
 # joining them up on gsp_name to get readable name
-<<<<<<< HEAD
+
 joined_df = new_df.merge(old_df, how='left', on=['gsp_name'])
 print(joined_df.head())
 
@@ -26,14 +26,14 @@ print('subset removed')
 joined_df.to_csv('GSPConsumer/gsp_name_update/gsp_new_ids_and_names.csv')
 print(joined_df.head())
 
-=======
+
 joined_df = new_df.merge(old_df, how="left", on=["gsp_name"])
 
 # dropping duplicates from the joined file
-joined_df.drop_duplicates(subset=None, keep="first", inplace=False)
+joined_df.drop_duplicates(subset=['gsp_id_x'])
 print(joined_df.head())
 
 joined_df.to_csv("GSPConsumer/gsp_name_update/gsp_new_ids_and_names.csv")
->>>>>>> 86779a6aaaea71a3b9cde3ef23c9d4cd5129e317
+
 
 print("finished")
