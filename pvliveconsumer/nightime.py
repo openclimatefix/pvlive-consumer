70ce63ce230c89d147929788a1d979b1dd744a34
peterdudfield <peter.dudfield@hotmail.com>
2024-12-18 17:27:06 +0000

    add import

--- pvliveconsumer/nightime.py
+++ pvliveconsumer/nightime.py
@@ -1,8 +1,7 @@
 """ Add zeros to nightime """
 import logging
 import os
-from datetime import datetime
-
+from datetime import datetime, timedelta
 import pandas as pd
 import pvlib
 from nowcasting_datamodel.models.gsp import LocationSQL
