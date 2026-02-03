1f5b63bce7e161fb022e97e9869c05c8f459e116
Peter Dudfield <peter.dudfield@hotmail.com>
2026-02-03 22:08:28 +0000

    use try and except

--- pvliveconsumer/app.py
+++ pvliveconsumer/app.py
@@ -20,6 +20,7 @@ from nowcasting_datamodel.models.base import Base_Forecast
 from nowcasting_datamodel.models.gsp import GSPYield, GSPYieldSQL, LocationSQL
 from pvlive_api import PVLive
 from sqlalchemy.orm import Session
+from pvlive_api.pvlive import PVLiveException
 
 import pvliveconsumer
 from pvliveconsumer.backup import make_gsp_yields_from_national
@@ -44,13 +45,14 @@ pvlive_domain_url = os.getenv("PVLIVE_DOMAIN_URL", "api.pvlive.uk")
 # ignore these gsp ids from PVLive as they are no longer used
 ignore_gsp_ids = [5, 17, 53, 75, 139, 140, 143, 157, 163, 225, 310]
 
-# These GSPs have been split, as part of PVLive update  20251204
+# These GSPs have been split, as part of PVLive update 20251204
 # To make this backwards compatible, we need to also save values for
 # IVER_1|IVER_6 158,
 # BRLE_1|FLEE_1 41
 # SEAB1|SAFO_1 257
-split_gsp_ids = {41 : [343,344], 158 : [345,346],  257 : [347,348]}
-    
+# Note that once, the platform doesnt need these old gsp ids, then we can put them into the ignore list
+split_gsp_ids = {41: [343, 344], 158: [345, 346], 257: [347, 348]}
+
 
 @click.command()
 @click.option(
@@ -184,26 +186,7 @@ def pull_data_and_save(
         if gsp.gsp_id in ignore_gsp_ids:
             continue
 
-        if gsp.gsp_id in split_gsp_ids:
-            logger.info(f"Summing up GSP ID {gsp.gsp_id} from gsp ids {split_gsp_ids[gsp.gsp_id]} parts")
-            gsp_ids = split_gsp_ids[gsp.gsp_id]
-            gsp_yield_dfs = []
-            for gsp_id in gsp_ids:
-                gsp_yield_df = pvlive.between(
-                    start=start,
-                    end=end,
-                    entity_type="gsp",
-                    entity_id=gsp_id,
-                    dataframe=True,
-                    extra_fields="installedcapacity_mwp,capacity_mwp,updated_gmt",
-                )
-                gsp_yield_dfs.append(gsp_yield_df)
-            gsp_yield_df = pd.concat(gsp_yield_dfs)
-            # sum up all these values
-            gsp_yield_df = gsp_yield_df.groupby("datetime_gmt").sum().reset_index()
-            gsp_yield_df['gsp_id'] = gsp.gsp_id
-
-        else:
+        try:
             gsp_yield_df: pd.DataFrame = pvlive.between(
                 start=start,
                 end=end,
@@ -212,6 +195,28 @@ def pull_data_and_save(
                 dataframe=True,
                 extra_fields="installedcapacity_mwp,capacity_mwp,updated_gmt",
             )
+        except PVLiveException as e:
+            if gsp.gsp_id in split_gsp_ids:
+                logger.info(f"Summing up GSP ID {gsp.gsp_id} from gsp ids {split_gsp_ids[gsp.gsp_id]} parts")
+                gsp_ids = split_gsp_ids[gsp.gsp_id]
+                gsp_yield_dfs = []
+                for gsp_id in gsp_ids:
+                    gsp_yield_df = pvlive.between(
+                        start=start,
+                        end=end,
+                        entity_type="gsp",
+                        entity_id=gsp_id,
+                        dataframe=True,
+                        extra_fields="installedcapacity_mwp,capacity_mwp,updated_gmt",
+                    )
+                    gsp_yield_dfs.append(gsp_yield_df)
+                gsp_yield_all_df = pd.concat(gsp_yield_dfs)
+                # sum up all these values
+                gsp_yield_df = gsp_yield_all_df.groupby("datetime_gmt").sum().reset_index()
+                gsp_yield_df['gsp_id'] = gsp.gsp_id
+                gsp_yield_df["updated_gmt"] = gsp_yield_dfs[0]["updated_gmt"]
+            else:
+                raise e
 
         logger.debug(f"Processing GSP ID {gsp.gsp_id} ({gsp.label}), out of {len(gsps)}")
 
